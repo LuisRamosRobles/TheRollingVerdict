8aae96e9fc5eee7c361e4caffde4b2d1adc4570e
Luis Ramos Robles <lrrzafiro6@gmail.com>
2024-12-09 00:10:39 +0100

    Añadiendo alias Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\CheckAdminRole;
+use App\Http\Middleware\CheckUserRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+
+        $middleware->alias([
+            'guest' => App\Http\Middleware\RedirectIfAuthenticated::class,
+            'user' => CheckUserRole::class,
+            'admin' => CheckAdminRole::class, // Add this line for admin middleware'
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
