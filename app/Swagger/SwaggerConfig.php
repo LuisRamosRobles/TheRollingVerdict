1daabf6f123e64cb8e00401866936944819ed692
Luis Ramos Robles <lrrzafiro6@gmail.com>
2024-12-09 17:43:43 +0100

    Añadiendo Swagger

---
+++ app/Swagger/SwaggerConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Swagger;
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="TheRollingVerdict",
+ *     description="Documentación de la API para el TFG",
+ *     @OA\Contact(
+ *         email="lrrzafiro6@gmail.com"
+ *     )
+ * )
+ * @OA\Server(
+ *     url="http://swagger.local",
+ *     description="Servidor local"
+ * )
+ */
+class SwaggerConfig
+{
+}
